dd5132fd995440a244762393c05703e2e6d7cb00
sugriv <sug.github@gmail.com>
2017-10-31 20:40:39 +0530

    page decorated successfully

--- onlineshopping/src/main/java/info/sugriv/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/info/sugriv/onlineshopping/controller/PageController.java
@@ -13,33 +13,27 @@ public class PageController {
 	public ModelAndView index() {
 
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "welcome to java spring web");
+		mv.addObject("title", "home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
 
-	@RequestMapping(value = "/test")
-	public ModelAndView test(@RequestParam(value="greeting",required=false) String greeting) {
-		
-		if(greeting==null) {
-			greeting="hello there!!";
-		}
-		
-		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", greeting);
-		return mv;
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
 
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
 	}
-	@RequestMapping(value = "/test/{greeting}")
-	public ModelAndView testPath(@PathVariable("greeting") String greeting) {
-		
-		if(greeting==null) {
-			greeting="hello there!!";
-		}
-		
-		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", greeting);
-		return mv;
 
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
 	}
 
 }
